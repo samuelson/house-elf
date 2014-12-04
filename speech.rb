1d44573a2592d12a93f27d41567027d4b457a6c7
Josh Samuelson <js@puppetlabs.com>
2014-12-04 13:28:45 -0800

    Initial commit

---
+++ speech.rb
@@ -0,0 +1,43 @@
+require 'pocketsphinx-ruby' # Omitted in subsequent examples
+
+listening_keyword = "House elf"
+
+command_list = {
+  "living room light" => "pl a1 on",
+  "Turn off living room light" => "pl a1 off",
+  "bedroom light" => "pl a2 on",
+  "Turn off bedroom light" => "pl a2 off",
+  "Play music" => "PM",
+  "Stop music" => "SM",
+	"Forget it" => "Forgotten"
+}
+
+configuration = Pocketsphinx::Configuration::Grammar.new do
+	sentence listening_keyword
+	command_list.each do |key, value|
+		sentence key
+	end
+end
+
+puts "Availible Commands:"
+command_list.each do |key, value|
+		puts key
+end
+
+recognizer = Pocketsphinx::LiveSpeechRecognizer.new(configuration)
+
+
+#By default don't listen for anything but
+listening = false
+recognizer.recognize do |speech|
+	if listening then
+		puts command_list[speech]
+		puts speech
+
+		#Stop listening after the command is done
+		listening = false
+	end
+
+	#If you hear the word, start listening for the next round
+	listening = true if speech == listening_keyword
+end
